7c6f0cfe10b55557db690ed1ad2206586f34625f
NKadish <noahkadish@hotmail.com>
2020-11-09 20:07:57 +0000

    Testing a while loop to ask questions and seeing what happens when lives get to 0

--- main.rb
+++ main.rb
@@ -16,11 +16,28 @@ player2 = Players.new("Player 2")
 # puts player1.lives
 # puts player2.lives
 
-question = Questions.new
-puts question.question
-answer = gets.chomp.to_i
-if answer == question.answer
-  puts "Good Job!"
-else 
-  puts "Bad Job!"
+# question = Questions.new
+# puts question.question
+# answer = gets.chomp.to_i
+# if answer == question.answer
+#   puts "Good Job!"
+# else 
+#   puts "Bad Job!"
+# end
+
+while player1.lives > 0 && player2.lives > 0 
+  question = Questions.new
+  puts question.question
+  print ">"
+  answer = gets.chomp.to_i
+  if answer == question.answer
+    puts "Good Job!"
+  else 
+    puts "Bad Job!"
+    player1.lives -= 3
+  end
+end 
+
+if player1.lives === 0 
+  puts "Game over, player one loses!"
 end
