cbbadef0a4a6d0605cd4eca91bf3dc3e2db84b75
NKadish <noahkadish@hotmail.com>
2020-11-09 19:23:04 +0000

    Add players.rb and main.rb, for now just making sure life totals work properly

---
+++ main.rb
@@ -0,0 +1,8 @@
+require "./players"
+
+players = Players.new
+player1 = players.player1
+player2 = players.player2
+
+puts "Player one life: #{player1}/3"
+puts "Player two life: #{player2}/3"
---
+++ players.rb
@@ -0,0 +1,14 @@
+class Players
+
+  attr_accessor :life
+  attr_reader :name
+
+  def player1
+    @lives = 3
+  end 
+
+  def player2
+    @lives = 3
+  end
+
+end
